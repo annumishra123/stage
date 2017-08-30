372a810d6ea6b759724729ab80ef6e93f079ab52
Shivi Ojha <sharvendu@yahoo.com>
2017-08-30 14:04:28 +0530

    Removed Demo Code

--- client/routes.js
+++ client/routes.js
@@ -16,8 +16,7 @@ if (typeof require.ensure !== 'function') {
  */
 if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
-  require('./modules/Post/pages/PostListPage/PostListPage');
-  require('./modules/Post/pages/PostDetailPage/PostDetailPage');
+  //require('./modules/Post/pages/PostListPage/PostListPage');
 }
 
 // react-router setup with code-splitting
