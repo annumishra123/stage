e3357b728ac058b15aabddd996861be9199aca86
Shivi Ojha <sharvendu@yahoo.com>
2017-11-01 19:28:40 +0530

    chnaged config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://mongo:27017/stage3-admin',
   port: process.env.PORT || 8000,
   access_token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDEwNjkxNjUsInVzZXJfbmFtZSI6ImJhY2tlbmRhZG1pbkBzdGFnZTMuY28iLCJhdXRob3JpdGllcyI6WyJST0xFX0FOT05ZTU9VUyIsIlJPTEVfVVNFUiIsIlJPTEVfQURNSU4iXSwianRpIjoiYjllYWIxZmYtMmFmMC00ZDI4LTlmZGQtNmQzMjgzYjdmMTMxIiwiY2xpZW50X2lkIjoiZWRnZSIsInNjb3BlIjpbInBhc3N3b3JkIl19.1_vAu7S8nAjgsUojqgZyuqI0gjtgIngTQr42TCdXapY',
   targetURL: 'https://staging.stage3.co'
