014b504b1d1d32804799ffb70678c2c8b54e472a
Unknown <ovais@stage3.co>
2017-11-10 13:12:20 +0530

    Chekcked customer details for shipping info

--- client/modules/Shop/components/CreateShopOrder.js
+++ client/modules/Shop/components/CreateShopOrder.js
@@ -61,128 +61,130 @@ class CreateShopOrder extends React.Component {
   renderProductPreview() {
     if (this.props.productDetail) {
       return <div>
-               <img src={ this.props.productDetail.frontimage } style={ { width: '200px' } } />
-               <p>
-                 Designer:
-                 { ' ' + this.props.productDetail.designer }
-               </p>
-               <p>
-                 Name:
-                 { ' ' + this.props.productDetail.name }
-               </p>
-               <p>
-                 Sale Price:
-                 { ' ' + this.props.productDetail.saleprice }
-               </p>
-               <p>
-                 Original Price:
-                 { ' ' + this.props.productDetail.originalretailprice }
-               </p>
-               <a target="blank" href={ clientConfig.targetURL + '/shop/product/' + this.props.productDetail.id }>View Complete Product</a>
-               <br/>
-               <br/>
-               <button onClick={ this.addProductToCart.bind(this) }>Add Product</button>
-               <br/>
-               <br/>
-             </div>
+        <img src={this.props.productDetail.frontimage} style={{ width: '200px' }} />
+        <p>
+          Designer:
+                 {' ' + this.props.productDetail.designer}
+        </p>
+        <p>
+          Name:
+                 {' ' + this.props.productDetail.name}
+        </p>
+        <p>
+          Sale Price:
+                 {' ' + this.props.productDetail.saleprice}
+        </p>
+        <p>
+          Original Price:
+                 {' ' + this.props.productDetail.originalretailprice}
+        </p>
+        <a target="blank" href={clientConfig.targetURL + '/shop/product/' + this.props.productDetail.id}>View Complete Product</a>
+        <br />
+        <br />
+        <button onClick={this.addProductToCart.bind(this)}>Add Product</button>
+        <br />
+        <br />
+      </div>
     }
   }
 
   renderCart() {
     if (this.props.shopPricing && Object.keys(this.props.shopPricing.linePricing).length > 0) {
       return <div>
-               <h3>Order Summary</h3>
-               <hr/>
-               <div>
-                 <table>
-                   <thead>
-                     <tr>
-                       <th style={ { width: '100px' } }>SKU</th>
-                       <th style={ { width: '100px' } }>Original Price</th>
-                       <th style={ { width: '100px' } }>Sale Price</th>
-                       <th style={ { width: '100px' } }></th>
-                     </tr>
-                   </thead>
-                   <tbody>
-                     { Object.keys(this.props.shopPricing.linePricing).map((item, i) => {
-                         return <tr key={ i }>
-                                  <td>
-                                    { this.props.shopPricing.linePricing[item].productId }
-                                  </td>
-                                  <td>
-                                    { this.props.shopPricing.linePricing[item].totalOriginalPrice }
-                                  </td>
-                                  <td>
-                                    { this.props.shopPricing.linePricing[item].totalDiscountedPrice }
-                                  </td>
-                                  <td>
-                                    <button onClick={ this.removeProductFromCart.bind(this, item) }>Remove Product</button>
-                                  </td>
-                                </tr>;
-                       }, this) }
-                   </tbody>
-                 </table>
-               </div>
-               <br/>
-               <p>Total Original Price:
-                 { ' ' + this.props.shopPricing.totalOriginalPrice }
-               </p>
-               <p>Total Sale Price:
-                 { ' ' + this.props.shopPricing.totalDiscountedPrice }
-               </p>
-               <br/>
-               <select onChange={ this.changePaymentMethod.bind(this) }>
-                 <option value="">-- Select Payment Method --</option>
-                 { clientConfig.paymentMethods.map((method, i) => {
-                     return <option key={ i } value={ method }>
-                              { method }
-                            </option>;
-                   }) }
-               </select>
-               <button onClick={ this.placeOrder.bind(this) }>Place Order</button>
-             </div>
+        <h3>Order Summary</h3>
+        <hr />
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ width: '100px' }}>SKU</th>
+                <th style={{ width: '100px' }}>Original Price</th>
+                <th style={{ width: '100px' }}>Sale Price</th>
+                <th style={{ width: '100px' }}></th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(this.props.shopPricing.linePricing).map((item, i) => {
+                return <tr key={i}>
+                  <td>
+                    {this.props.shopPricing.linePricing[item].productId}
+                  </td>
+                  <td>
+                    {this.props.shopPricing.linePricing[item].totalOriginalPrice}
+                  </td>
+                  <td>
+                    {this.props.shopPricing.linePricing[item].totalDiscountedPrice}
+                  </td>
+                  <td>
+                    <button onClick={this.removeProductFromCart.bind(this, item)}>Remove Product</button>
+                  </td>
+                </tr>;
+              }, this)}
+            </tbody>
+          </table>
+        </div>
+        <br />
+        <p>Total Original Price:
+                 {' ' + this.props.shopPricing.totalOriginalPrice}
+        </p>
+        <p>Total Sale Price:
+                 {' ' + this.props.shopPricing.totalDiscountedPrice}
+        </p>
+        <br />
+        <select onChange={this.changePaymentMethod.bind(this)}>
+          <option value="">-- Select Payment Method --</option>
+          {clientConfig.paymentMethods.map((method, i) => {
+            return <option key={i} value={method}>
+              {method}
+            </option>;
+          })}
+        </select>
+        <button onClick={this.placeOrder.bind(this)}>Place Order</button>
+      </div>
     }
   }
 
   renderCustomerDetails() {
-    let address = this.props.customerDetail.shippingInfo.find(x => x.shippingId == this.props.selectedAddress);
-    return <div>
-             <h3>Customer Details</h3>
-             <br/>
-             <p> Name:
-               { ' ' + this.props.customerDetail.firstName + ' ' + this.props.customerDetail.lastName }
-             </p>
-             <p> Email:
-               { ' ' + this.props.customerDetail.email }
-             </p>
-             <p> Phone Number:
-               { ' ' + this.props.customerDetail.phoneNumber }
-             </p>
-             <br/>
-             <h4>Selected Address:</h4>
-             <p>
-               { address.address + ',' }
-             </p>
-             <p>
-               { address.city + ', ' + address.state + ' - ' + address.pincode }
-             </p>
-             <br/>
-           </div>
+    if (this.props.customerDetail) {
+      let address = this.props.customerDetail.shippingInfo.find(x => x.shippingId == this.props.selectedAddress);
+      return <div>
+        <h3>Customer Details</h3>
+        <br />
+        <p> Name:
+               {' ' + this.props.customerDetail.firstName + ' ' + this.props.customerDetail.lastName}
+        </p>
+        <p> Email:
+               {' ' + this.props.customerDetail.email}
+        </p>
+        <p> Phone Number:
+               {' ' + this.props.customerDetail.phoneNumber}
+        </p>
+        <br />
+        <h4>Selected Address:</h4>
+        <p>
+          {address.address + ','}
+        </p>
+        <p>
+          {address.city + ', ' + address.state + ' - ' + address.pincode}
+        </p>
+        <br />
+      </div>
+    }
   }
 
   render() {
     return <section>
-             { this.renderCustomerDetails() }
-             <h3>Create New Shop Order</h3>
-             <br/>
-             <label>SKU: </label>
-             <input type="text" onChange={ this.handleChangeSKU.bind(this) } />
-             <button onClick={ this.findProduct.bind(this) }>Find</button>
-             <br/>
-             <br/>
-             { this.renderProductPreview() }
-             { this.renderCart() }
-           </section>
+      {this.renderCustomerDetails()}
+      <h3>Create New Shop Order</h3>
+      <br />
+      <label>SKU: </label>
+      <input type="text" onChange={this.handleChangeSKU.bind(this)} />
+      <button onClick={this.findProduct.bind(this)}>Find</button>
+      <br />
+      <br />
+      {this.renderProductPreview()}
+      {this.renderCart()}
+    </section>
   }
 }
 
