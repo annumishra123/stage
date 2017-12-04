57d4415981be7fb7e99cf9e422450ad3e6744a8b
Shivi Ojha <sharvendu@yahoo.com>
2017-12-04 13:27:19 +0530

    Created Invoice & Orders Component For Designer

--- client/modules/Designer/components/Invoice.jsx
+++ client/modules/Designer/components/Invoice.jsx
@@ -8,38 +8,25 @@ import { getDesignerInventory } from '../DesignerActions';
 import clientConfig from '../../../config';
 
 
-class Orders extends React.Component {
+class Invoice extends React.Component {
     constructor(props) {
         super(props);
     }
 
     componentDidMount() {
-        if (this.props.owner) {
-            this.props.getDesignerInventory(this.props.owner);
-        }
+        // if (this.props.owner) {
+        //     this.props.getDesignerInventory(this.props.owner);
+        // }
     }
 
     componentWillReceiveProps(nextProps) {
-        if (this.props.owner !== nextProps.owner) {
-            this.props.getDesignerInventory(nextProps.owner);
-        }
-    }
-
-    renderDesignerInventory() {
-        if (this.props.inventory) {
-            if (this.props.inventory.length > 0) {
-                return <div>
-                         <ReactTable filterable data={ this.props.inventory } columns={ clientConfig.designerInventoryColumns } defaultPageSize={ 10 } className="-striped -highlight" />
-                       </div>;
-            }
-        }
+        // if (this.props.owner !== nextProps.owner) {
+        //     this.props.getDesignerInventory(nextProps.owner);
+        // }
     }
 
     render() {
         return <section>
-                 <h1>Your Inventory</h1>
-                 <br/>
-                 { this.renderDesignerInventory() }
                </section>
     }
 }
@@ -60,4 +47,4 @@ function mapStateToProps(state) {
 }
 
 
-export default connect(mapStateToProps, matchDispatchToProps)(Orders);
+export default connect(mapStateToProps, matchDispatchToProps)(Invoice);
