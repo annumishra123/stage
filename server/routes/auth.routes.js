ee0bd0f1b67604069680a24290c5fa34bf13a645
Shivi Ojha <sharvendu@yahoo.com>
2017-12-06 23:50:28 +0530

    Change Password API

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import passport from '../passport';
 import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
 const router = new Router();
 
@@ -55,6 +56,107 @@ router.get('/me', passport.authenticate('jwt', {
             owner: req.user.owner
         },
     });
+});
+
+router.get("/getusers", passport.authenticate('jwt', {
+    session: false,
+}), (req, res) => {
+    if (req.user.role === 'superuser') {
+        User.find({}).then(users => {
+            res.json(users);
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json({
+                status: 'FAILED'
+            });
+        });
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
+
+router.post("/createuser", passport.authenticate('jwt', {
+    session: false,
+}), (req, res) => {
+    if (req.user.role === 'superuser') {
+        if (req.body.email && req.body.role && req.body.password && req.body.name) {
+            User.findOne({
+                'email': req.body.email
+            }, function(err, user) {
+                if (!user) {
+                    var newUser = new User({
+                        email: req.body.email,
+                        isAdmin: true,
+                        cuid: cuid(),
+                        role: req.body.role,
+                        name: req.body.name,
+                        owner: req.body.owner,
+                        dateAdded: Date.now()
+                    });
+                    newUser.password = newUser.generateHash(req.body.password);
+                    newUser.save().then(item => {
+                        res.json({
+                            status: 'SUCCESS'
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                        res.status(400).json({
+                            status: 'FAILED'
+                        });
+                    });
+                } else if (user) {
+                    user.set({
+                        role: req.body.role,
+                        password: user.generateHash(req.body.password)
+                    });
+                    user.save().then(item => {
+                        res.json({
+                            status: 'SUCCESS'
+                        });
+                    }).catch(err => {
+                        res.status(400).json({
+                            status: 'FAILED'
+                        });
+                    });
+                }
+            });
+        } else {
+            res.status(400).send('Bad Request');
+        }
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
+
+router.post("/changepassword", passport.authenticate('jwt', {
+    session: false,
+}), (req, res) => {
+    if (req.user) {
+        if (req.body.password) {
+            User.findOne({
+                'email': req.user.email
+            }, function(err, user) {
+                if (user) {
+                    user.set({
+                        password: user.generateHash(req.body.password)
+                    });
+                    user.save().then(item => {
+                        res.json({
+                            status: 'SUCCESS'
+                        });
+                    }).catch(err => {
+                        res.status(400).json({
+                            status: 'FAILED'
+                        });
+                    });
+                }
+            });
+        } else {
+            res.status(400).send('Bad Request');
+        }
+    } else {
+        res.status(401).send('Unauthorized');
+    }
 });
 
 export default router;
