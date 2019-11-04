3e0ae995ea8c0df92bbabc10dae76a71af28f0a5
OvaisRizwan <ovais@stage3.co>
2019-11-04 12:58:52 +0530

    New access token added

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/stage3-admin',
   port: process.env.PORT || 8000,
-  access_token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzI2MDcyNDMsInVzZXJfbmFtZSI6ImJhY2tlbmRhZG1pbkBzdGFnZTMuY28iLCJhdXRob3JpdGllcyI6WyJST0xFX0FOT05ZTU9VUyIsIlJPTEVfVVNFUiIsIlJPTEVfQURNSU4iXSwianRpIjoiYzc2MTMwYjItZGU5NC00YmM5LTlhNTktOWUxNTlmNWNiYzQxIiwiY2xpZW50X2lkIjoiZWRnZSIsInNjb3BlIjpbInBhc3N3b3JkIl19.MQfxuV5lx1Os8KtCnVk2_YNyljlnoa1BO2nKrqILU0Q',
+  access_token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQxNDUzMDIsInVzZXJfbmFtZSI6ImJhY2tlbmRhZG1pbkBzdGFnZTMuY28iLCJhdXRob3JpdGllcyI6WyJST0xFX0FOT05ZTU9VUyIsIlJPTEVfVVNFUiIsIlJPTEVfQURNSU4iXSwianRpIjoiYTIwMWNiYzctYmExZC00YjYxLWE4NGUtNDhmZTk0Mjk3ZjFjIiwiY2xpZW50X2lkIjoiZWRnZSIsInNjb3BlIjpbInBhc3N3b3JkIl19.NJrr29bgOVjdVJ6cfiWQdKRE_Y8QZg4a0YXJeMbRB9E',
   targetURL: 'https://staging.stage3.co',
   notificationUrl: 'https://notification-test.stage3.co'
 };
