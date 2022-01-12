1196d60a5d4135a955da58e25097b8c559bed327
Shivi Ojha <sharvendu@yahoo.com>
2022-01-12 15:39:03 +0530

    Local Mongo URL

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://mongo:27017/stage3-admin',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/stage3-admin',
   port: process.env.PORT || 8000,
   access_token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjczMDIxODMsInVzZXJfbmFtZSI6ImJhY2tlbmRhZG1pbkBzdGFnZTMuY28iLCJhdXRob3JpdGllcyI6WyJST0xFX0FOT05ZTU9VUyIsIlJPTEVfVVNFUiIsIlJPTEVfQURNSU4iXSwianRpIjoiYjViNDk3M2MtN2Q4MC00MWVlLWEyNmQtODEzYWVjZjAwZTA0IiwiY2xpZW50X2lkIjoiZWRnZSIsInNjb3BlIjpbInBhc3N3b3JkIl19.NLEnFbJ9rtaM1YP5h2zps4XUwEaUaVw4qlz5jsauiPE',
   targetURL: 'https://staging.stage3.co',
